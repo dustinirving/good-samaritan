63b18aa9d0bea61b6a95453259c6ffa0d0d1d67b
Jesse Okeya <Jesseokeya@gmail.com>
2020-05-16 13:08:28 -0400

    changes

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const router = require('express').Router()
-
 // Routes
 // =============================================================
 router.get('/', function (req, res) {
@@ -10,11 +9,17 @@ router.get('/', function (req, res) {
 router.get('/help', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/help.html'))
 })
+
 router.get('/signup', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/signup.html'))
 })
+
 router.get('/details', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/details.html'))
+})
+
+router.get('/chat', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/chat.html'))
 })
 
 module.exports = router
---
+++ public/js/chat.js
@@ -0,0 +1,51 @@
+const socket = io('http://localhost:3000')
+
+let wasPreviousIn = false
+
+const registerSocket = () => {
+  socket.on('news', (data) => {
+    console.log(data)
+    socket.emit('my other event', { my: 'data' })
+  })
+}
+
+const chatMessage = ({ message, name }) => {
+  return `<li class="${wasPreviousIn ? 'out' : 'in'}">
+    <div class="chat-img" bis_skin_checked="1">
+        <img alt="Avtar" src="https://bootdey.com/img/Content/avatar/avatar3.png">
+    </div>
+    <div class="chat-body" bis_skin_checked="1">
+        <div class="chat-message" bis_skin_checked="1">
+            <h5>${name}</h5>
+            <p>${message}</p>
+        </div>
+    </div>
+</li>`
+}
+
+const addChatMessage = (message) => {
+  const entry = document.querySelector('.chat-list')
+  socket.emit('my other event', { message })
+  const context = {
+    name: 'Jesse',
+    message
+  }
+  entry.innerHTML += chatMessage(context)
+  wasPreviousIn = !wasPreviousIn
+}
+
+const handleChange = (e) => {
+  const message = e.target.value
+  if (e.key === 'Enter') {
+    addChatMessage(message)
+    e.target.value = ''
+  }
+}
+
+const entryPoint = () => {
+  const inputField = document.querySelector('#chatInput')
+  inputField.addEventListener('keyup', handleChange)
+  registerSocket()
+}
+
+document.addEventListener('DOMContentLoaded', entryPoint)
