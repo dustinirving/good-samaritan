e2f9a2e6c1462d77e226c06c68a5235d13dbbc87
Dustin Irving <dustin.irving@gmail.com>
2020-05-16 00:45:39 -0400

    Set up basic routes

---
+++ controllers/html-routes.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const router = require('express').Router()
+
+// Routes
+// =============================================================
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+router.get('/help', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/help.html'))
+})
+router.get('/signup', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/signup.html'))
+})
+router.get('/details', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/details.html'))
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,36 @@
+// ****************************************************************************
+// Server.js
+// This file is the initial starting point for the Node/Express server.
+// ****************************************************************************
+
+// Dependencies
+// =============================================================
+const express = require('express')
+
+// Requiring our models for syncing to the MySQL database
+// Remember: This syntax imports the `db` object exported from the
+// `./models/index.js` module.
+const db = require('./models')
+
+// Sets up the Express App
+// =============================================================
+const app = express()
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Allow Express to automatically serve static resource like the
+// HTML, CSS and JavaScript for the frontend client application.
+app.use(express.static('./public'))
+
+// Routes
+// =============================================================
+app.use(require('./controllers/html-routes.js'))
+// app.use('/api', require('./controllers/api-routes.js'))
+
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync({ force: true }).then(() => {
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
+})
