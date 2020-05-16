402d0f158cbf4a2ef1aa184aeefa2aacf98b8c44
Larry_A <larryagbana@gmail.com>
2020-05-16 15:22:19 -0400

    client side geolocator

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+document.querySelector('#helpBtn').addEventListener('click', function () {
+  function locator () {
+    function executor (resolve, reject) {
+      window.navigator.geolocation.getCurrentPosition(
+        position => resolve(position),
+        err => reject(err)
+      )
+    }
+
+    return new Promise(executor)
+  }
+  locator().then(data => {
+    fetch(`/api/patients`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({lat: data.coords.latitude, long: data.coords.longitude})
+    }).then(response => {
+      if (response.ok) location.reload()
+    })
+  })
+})
