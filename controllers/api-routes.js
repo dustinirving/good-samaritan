d17bd397e93f6f1d39b2c29795532135bc566093
Larry_A <larryagbana@gmail.com>
2020-05-16 20:18:51 -0400

    server side locator

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,5 +1,6 @@
 const db = require('../models')
 const router = require('express').Router()
+const iplocate = require('node-iplocate')
 
 router.get('/volunteers', function (req, res) {
   db.Volunteer.findAll()
@@ -19,8 +20,23 @@ router.post('/volunteers', function (req, res) {
     })
 })
 
-router.post('/patients', function (req, res) {
-  db.Patient.create(req.body)
+router.post('/patients', async function (req, res) {
+  async function ip (req) {
+    const address = 
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    if (address && address.includes(",")) {
+      return iplocate(address.split(",")[0].trim());
+    }
+    return await iplocate(address);
+  }
+
+  let body = req.body
+  let {latitude, longitude} = await ip(req)
+
+  db.Patient.create({...body, latitude, longitude})
     .then(patient => res.status(201).json({ data: patient }))
     .catch(err => {
       console.log(err)
