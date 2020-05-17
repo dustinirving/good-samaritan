154acf542d6143f676effa6d82733f1ccaa0c22f
Jesse Okeya <Jesseokeya@gmail.com>
2020-05-17 02:39:35 -0400

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,38 @@
-const socket = window.io()
+document.addEventListener('DOMContentLoaded', () => {
+  const socket = window.io()
 
-document.querySelector('#helpBtn').addEventListener('click', function () {
-  function locator () {
-    function executor (resolve, reject) {
-      return window.navigator.geolocation.getCurrentPosition(
-        position => resolve(position),
-        err => { if (err) reject(err) }
-      )
+  document.querySelector('#helpBtn').addEventListener('click', function () {
+    function locator() {
+      function executor(resolve, reject) {
+        return window.navigator.geolocation.getCurrentPosition(
+          position => resolve(position),
+          err => {
+            if (err) reject(err)
+          }
+        )
+      }
+
+      return new Promise(executor)
     }
 
-    return new Promise(executor)
-  }
+    socket.on('recieveNotification', (data) => {
+      const user = localStorage.getItem('user')
+      if (user && data.userToSendNotification) {
+        console.log(data)
+      }
+    })
 
-  socket.on('recieveNotification', (data) => {
-    const user = localStorage.getItem('user')
-    if (user && data.userToSendNotification) {
-      console.log(data)
-    }
-  })
-
-  locator().then(data => {
-    fetch('/api/volunteers/alert', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ lat: data.coords.latitude, long: data.coords.longitude })
-    }).then(response => response.json()).then(res => socket.emit('sendNotification', res))
+    locator().then(data => {
+      fetch('/api/volunteers/alert', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          lat: data.coords.latitude,
+          long: data.coords.longitude
+        })
+      }).then(response => response.json()).then(res => socket.emit('sendNotification', res))
+    })
   })
 })
