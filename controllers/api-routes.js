0dc833852c3104c0c7296e5092137f6be7bd2c54
Larry_A <larryagbana@gmail.com>
2020-05-16 20:21:03 -0400

    server side geolocator

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -22,21 +22,20 @@ router.post('/volunteers', function (req, res) {
 
 router.post('/patients', async function (req, res) {
   async function ip (req) {
-    const address = 
-      req.headers["x-forwarded-for"] ||
+    const address = req.headers['x-forwarded-for'] ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
-      (req.connection.socket ? req.connection.socket.remoteAddress : null);
-    if (address && address.includes(",")) {
-      return iplocate(address.split(",")[0].trim());
+      (req.connection.socket ? req.connection.socket.remoteAddress : null)
+    if (address && address.includes(',')) {
+      return iplocate(address.split(',')[0].trim())
     }
-    return await iplocate(address);
+    return await iplocate(address)
   }
 
-  let body = req.body
-  let {latitude, longitude} = await ip(req)
+  const body = req.body
+  const { latitude, longitude } = await ip(req)
 
-  db.Patient.create({...body, latitude, longitude})
+  db.Patient.create({ ...body, latitude, longitude })
     .then(patient => res.status(201).json({ data: patient }))
     .catch(err => {
       console.log(err)
