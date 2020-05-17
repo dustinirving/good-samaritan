8ef8a21f840f574a4ac5a2db327c3d3d975e7341
Jesse Okeya <Jesseokeya@gmail.com>
2020-05-16 20:35:26 -0400

    changes

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -2,6 +2,18 @@ const db = require('../models')
 const router = require('express').Router()
 const iplocate = require('node-iplocate')
 
+const ip = async (req) => {
+  const address =
+    req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null)
+  if (address && address.includes(',')) {
+    return iplocate(address.split(',')[0].trim())
+  }
+  return iplocate(address)
+}
+
 router.get('/volunteers', function (req, res) {
   db.Volunteer.findAll()
     .then(volunteer => res.status(200).json({ data: volunteer }))
@@ -21,25 +33,14 @@ router.post('/volunteers', function (req, res) {
 })
 
 router.post('/patients', async function (req, res) {
-  async function ip (req) {
-    const address = req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      (req.connection.socket ? req.connection.socket.remoteAddress : null)
-    if (address && address.includes(',')) {
-      return iplocate(address.split(',')[0].trim())
-    }
-    return await iplocate(address)
-  }
-
   const body = req.body
-  const { latitude, longitude } = await ip(req)
-
-  db.Patient.create({ ...body, latitude, longitude })
+  const { longitude, latitude } = await ip(req)
+  db.Patient.create({ ...body, longitude, latitude })
     .then(patient => res.status(201).json({ data: patient }))
     .catch(err => {
       console.log(err)
       res.status(500).json({ errors: [err] })
     })
 })
+
 module.exports = router
